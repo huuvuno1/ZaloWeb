4ccda4bf07ad9075be458ffd9a762cd9bd086546
huuvuno1 <nguyenhuuvuno1@gmail.com>
2021-12-21 12:48:22 +0700

    deploy railway

--- src/configurations/db.config.js
+++ src/configurations/db.config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 module.exports = () => {
     const MONGO_HOST = process.env.MONGO_HOST || 'localhost'
     const MONGO_PORT = process.env.MONGO_PORT || 27017
-    const MONGO_DB = process.env.MONGO_DB || "AAAAAAAA"
+    const MONGO_DB = process.env.MONGO_DB || "zaloweb"
 
     // const mongoDbUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`
     const mongoDbUrl = `mongodb://mongo:n5vEgvQO0MPPWzCcPHLq@containers-us-west-2.railway.app:5455`
